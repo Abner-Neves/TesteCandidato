47b58a9decdfb805d1218ec2a2446904afe9a9ce
Abner Neves <abner.z.neves@gmail.com>
2024-05-19 11:51:37 -0300

    feat: Implementação da busca e validação de CEP
    -Criado script em js para a validação do cep utilizando a API ViaCep e preenchimento dos campos com o json
    -Tela privacy retirada

--- TesteCandidatoWEB/Controllers/HomeController.cs
+++ TesteCandidatoWEB/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace TesteCandidatoWEB.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ TesteCandidatoWEB/wwwroot/js/buscaCep.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    $('#cepInput').change(function () {
+        var cep = $(this).val().replace(/\D/g, '');
+        if (cep) {
+            var validCep = /^[0-9]{8}$/;
+            if (validCep.test(cep)) {
+                $.ajax({
+                    url: `https://viacep.com.br/ws/${cep}/json/`,
+                    dataType: "json",
+                    success: function (data) {
+                        if (data.erro) {
+                            alert("CEP inválido. Por favor, tente novamente.");
+                            $('#cepInput').focus();
+                            return;
+                        }
+                        $('#logradouroInput').val(data.logradouro);
+                        $('#complementoInput').val(data.complemento);
+                        $('#bairroInput').val(data.bairro);
+                        $('#localidadeInput').val(data.localidade);
+                        $('#ufInput').val(data.uf);
+                        $('#unidadeInput').val(data.unidade);
+                        $('#ibgeInput').val(data.ibge);
+                        $('#giaInput').val(data.gia);
+                    },
+                    error: function () {
+                        alert("Erro ao buscar o CEP. Por favor, tente novamente.");
+                        $('#cepInput').focus();
+                    }
+                });
+            } else {
+                alert("Formato de CEP inválido.");
+                $('#cepInput').focus();
+            }
+        } else {
+            alert("Por favor, informe um CEP.");
+            $('#cepInput').focus();
+        }
+    });
+});
